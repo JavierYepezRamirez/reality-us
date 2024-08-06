1ff35136030385f674b74fce6ccee0d38c0d5734
unknown <javier_yepez@outlook.com>
2024-08-06 09:29:13 -0600

    Agregamos en el endPoint para buscar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 console.log('@@@ Conectado...')
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadData(city = null)
+})
+
+const loadData = async (city) => {
+	if(!city) {
+		city = 'new york'
+	}
+
+	const url = `https://realty-in-us.p.rapidapi.com/locations/auto-complete?input=${city}&limit=5`;
+	const options = {
+		method: 'GET',
+		headers: {
+			'x-rapidapi-key': 'bf8d17ce4amsh8fc500f384c7217p1c265cjsn2646ea4303ca',
+			'x-rapidapi-host': 'realty-in-us.p.rapidapi.com'
+		}
+	};
+
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json();
+		console.log('@@@ result =>', result);
+	} catch (error) {
+		console.error(error);
+	}
+}
